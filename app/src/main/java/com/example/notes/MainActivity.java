e21436fb89f53a2cb7fad2e3e49d3104ea779bd4
edwin001-tech <edwinshitakwa8@gmail.com>
2020-06-10 10:59:56 +0300

    created NoteEditorActivity intent

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example note");
+        //adapter converts an ArrayList of objects into View items loaded into the ListView container.
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("note Id", position);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
